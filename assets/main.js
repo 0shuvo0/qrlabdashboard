c0a662e377845a99953aaa188da4a7d2cccc98c6
0shuvo0 <seriouslolz1@gmail.com>
2022-11-27 13:45:22 +0600

    finished start qr codes page

--- assets/main.js
+++ assets/main.js
@@ -2,14 +2,22 @@ const $ = (v, p = document) => p.querySelector(v)
 const $$ = (v, p = document) => [...p.querySelectorAll(v)]
 
 
-const qrCodeEditBtns = $$('.generated-link-item .badge-btn.edit .popup-btn-link:first-child')
+const qrCodeEditInfoBtns = $$('.generated-link-item .badge-btn.edit .popup-btn-link:first-child')
+const qrCodeEditDesignBtns = $$('.generated-link-item .badge-btn.edit .popup-btn-link:nth-child(2)')
+const qrCodeDeleteDesignBtns = $$('.generated-link-item .badge-btn.edit .popup-btn-link:nth-child(3)')
 const editInfoModal= $('.edit-info-modal-wrapper')
+const editDesignModal= $('.edit-design-modal-wrapper')
 const deleteQRCodesModal= $('.delete-modal-wrapper')
 
 
 $$('.close-btn', editInfoModal).forEach(btn => {
     btn.addEventListener('click', () => {
         editInfoModal.classList.add('hidden')
+    })
+})
+$$('.close-btn', editDesignModal).forEach(btn => {
+    btn.addEventListener('click', () => {
+        editDesignModal.classList.add('hidden')
     })
 })
 $$('.close-btn', deleteQRCodesModal).forEach(btn => {
@@ -20,9 +28,19 @@ $$('.close-btn', deleteQRCodesModal).forEach(btn => {
 
 
 
-qrCodeEditBtns.forEach(btn => {
+qrCodeEditInfoBtns.forEach(btn => {
     btn.addEventListener('click', () => {
         editInfoModal.classList.remove('hidden')
+    })
+})
+qrCodeEditDesignBtns.forEach(btn => {
+    btn.addEventListener('click', () => {
+        editDesignModal.classList.remove('hidden')
+    })
+})
+qrCodeDeleteDesignBtns.forEach(btn => {
+    btn.addEventListener('click', () => {
+        deleteQRCodesModal.classList.remove('hidden')
     })
 })
 
