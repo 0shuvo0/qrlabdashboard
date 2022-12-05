09eb1faa866b07f3345da8a3ea29a0753d8e75b9
0shuvo0 <seriouslolz1@gmail.com>
2022-12-05 10:45:52 +0600

    added remove nfc modal

---
+++ assets/js/dynamicnfctags.js
@@ -0,0 +1,84 @@
+const $ = (v, p = document) => p.querySelector(v)
+const $$ = (v, p = document) => [...p.querySelectorAll(v)]
+
+
+const qrCodeEditInfoBtns = $$('.generated-link-item .badge-btn.edit .popup-btn-link:nth-child(2)')
+const removeNFCBtns = $$('.generated-link-item .badge-btn.edit .popup-btn-link:nth-child(3)')
+const qrCodeDeleteBtns = $$('.generated-link-item .badge-btn.edit .popup-btn-link:nth-child(4), .delete-tag-btn')
+const manageTagsBtns = $$('.manage-tags-btn')
+
+const addNewFolderBtn = $('.add-new-folder-btn')
+const formBtns = $$('.form-btns .form-btn:not(.add-new-folder-btn')
+
+const editInfoModal= $('.edit-info-modal-wrapper')
+const deleteQRCodesModal= $('.delete-modal-wrapper')
+const addNewFolderModal= $('.add-new-folder-modal-wrapper')
+const manageTagsModal = $('.manage-tags-modal-wrapper')
+const removeNFCModal = $('.remove-nfc-modal-wrapper')
+
+
+$$('.modal-wrapper').forEach(modal => {
+    const closeBtns = $$('.close-btn', modal)
+    closeBtns.forEach(btn => {
+        btn.addEventListener('click', () => {
+            modal.classList.add('hidden')
+        })
+    })
+})
+
+qrCodeEditInfoBtns.forEach(btn => {
+    btn.addEventListener('click', () => {
+        editInfoModal.classList.remove('hidden')
+    })
+})
+qrCodeDeleteBtns.forEach(btn => {
+    btn.addEventListener('click', () => {
+        deleteQRCodesModal.classList.remove('hidden')
+    })
+})
+manageTagsBtns.forEach(btn => {
+    btn.addEventListener('click', () => {
+        manageTagsModal.classList.remove('hidden')
+    })
+})
+removeNFCBtns.forEach(btn => {
+    btn.addEventListener('click', () => {
+        removeNFCModal.classList.remove('hidden')
+    })
+})
+
+
+addNewFolderBtn.addEventListener('click', () => {
+    addNewFolderModal.classList.remove('hidden')
+})
+
+$('.info-item.delete').addEventListener('click', () => {
+    deleteQRCodesModal.classList.remove('hidden')
+})
+
+
+const resetSelectedFolder = folderName => {
+    formBtns.forEach(btn => btn.classList.remove('active'))
+    $$('.folder-list .folder-list-item').forEach(item => {
+        item.classList.remove('active')
+        if(($('.title', item)).innerText === folderName){
+            item.classList.add('active')
+        }
+    })
+}
+
+formBtns.forEach(btn => {
+    btn.addEventListener('click', () => {
+        const folderName = $('span', btn).innerText
+        resetSelectedFolder(folderName)
+        btn.classList.add('active')
+        $('.selected-folder-name').innerText = folderName
+    })
+})
+
+
+$('.select-all-qr-codes-btn').addEventListener('click', () => {
+    $('.when-no-selections').classList.add('hidden')
+    $('.when-selections').classList.remove('hidden')
+    $$('.generated-link-item').forEach(i => i.classList.add('active'))
+})
